a0eff1e745ce5bb050dc8200239db2a74c8aafa2
Rupjyoti Sarma <rup.sarma23@gmail.com>
2025-05-07 12:09:34 +0530

    initial commit

---
+++ src/main/java/com/example/resumeScreening/Be/ResumeScreeningBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeScreening.Be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeScreeningBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeScreeningBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resumeScreening/Be/ResumeScreeningBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeScreening.Be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeScreeningBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
